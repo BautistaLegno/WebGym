40d42d8d4037f942558402b8decc254032245345
PedroMestroni <135996379+PedroMestroni@users.noreply.github.com>
2023-06-15 08:47:48 -0300

    Version1
    Version inicial de la página

---
+++ Registro.js
@@ -0,0 +1,19 @@
+function Registro(){
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        var nombres = document.getElementById("nombres").value;
+        var correo = document.getElementById("correo").value;
+        var contraseña1 = document.getElementById("contraseña1").value;
+        var contraseña2 = document.getElementById("contraseña2").value;
+
+        if (nombres=="nombre" && correo=="correo@gmail.com"){
+            if (contraseña1=="123" && contraseña2==contraseña1){
+                window.location = "Login.html";
+            }else{
+                alert("Las contraseñas no son iguales");
+            }
+        }else{
+            alert("Algun dato es incompatible");
+        }
+    })
+}
